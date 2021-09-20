ed5545a5459561cae2e2130df6ae3f01b1b26493
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-09-20 17:20:19 +0200

    Initial commit

---
+++ service-starter/src/main/java/pl/lodz/p/it/gymbackend/GymBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.gymbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBackendApplication.class, args);
+    }
+
+}
