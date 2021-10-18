b8198bf85019f4b6235a45a890974f87a4a52173
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-10-18 23:20:43 +0200

    Add necessary annotations

--- service-starter/src/main/java/pl/lodz/p/it/gymbackend/servicestarter/GymBackendApplication.java
+++ service-starter/src/main/java/pl/lodz/p/it/gymbackend/servicestarter/GymBackendApplication.java
@@ -2,8 +2,13 @@ package pl.lodz.p.it.gymbackend.servicestarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}, scanBasePackages = "pl.lodz.p.it.*")
+@EnableJpaRepositories("pl.lodz.p.it.*")
+@EntityScan("pl.lodz.p.it.repositoryhibernate.entity")
 public class GymBackendApplication {
 
     public static void main(String[] args) {
