d9acfaf27f48b7adac8fdabcfcf3140072b70fe0
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-10-22 11:04:20 +0200

    Implement get and post methods for account, create required dtos, change dependencies order

--- adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/controller/AccountController.java
+++ adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/controller/AccountController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import pl.lodz.p.it.core.port.primary.AccountServicePort;
+import pl.lodz.p.it.restapi.dto.AccountDetailsResponse;
 import pl.lodz.p.it.restapi.dto.AccountGeneralResponse;
 import pl.lodz.p.it.restapi.dto.AccountRequest;
-import pl.lodz.p.it.restapi.mapper.AccountRestMapper;
+import pl.lodz.p.it.restapi.mapper.AccountDetailsResponseMapper;
+import pl.lodz.p.it.restapi.mapper.AccountGeneralResponseMapper;
+import pl.lodz.p.it.restapi.mapper.AccountRequestMapper;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,18 +20,32 @@ public class AccountController implements AccountsApiDelegate {
 
     private final AccountServicePort accountServicePort;
 
-    private final AccountRestMapper accountRestMapper;
+    private final AccountGeneralResponseMapper accountGeneralResponseMapper;
+
+    private final AccountDetailsResponseMapper accountDetailsResponseMapper;
+
+    private final AccountRequestMapper accountRequestMapper;
 
     @Override
-    public ResponseEntity<Void> createAccount(AccountRequest accountRequest) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<AccountGeneralResponse> createAccount(AccountRequest accountRequest) {
+        return ResponseEntity.ok(
+                accountGeneralResponseMapper.toDtoModel(
+                        accountServicePort.save(accountRequestMapper.toDomainModel(accountRequest)))
+        );
     }
 
     @Override
     public ResponseEntity<List<AccountGeneralResponse>> getAccounts() {
         return ResponseEntity.ok(
                 accountServicePort.findAll().stream()
-                        .map(accountRestMapper::toDtoModel)
+                        .map(accountGeneralResponseMapper::toDtoModel)
                         .collect(Collectors.toList()));
+    }
+
+    @Override
+    public ResponseEntity<AccountDetailsResponse> getAccount(String login) {
+        return ResponseEntity.ok(
+                accountDetailsResponseMapper.toDtoModel(
+                        accountServicePort.find(login)));
     }
 }
