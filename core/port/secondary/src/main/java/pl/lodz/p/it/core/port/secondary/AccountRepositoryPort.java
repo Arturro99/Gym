806e6b39a76e6615080e785b689086ca235d79a7
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-11-04 18:28:45 +0100

    Add methods associated with training plans and diets

--- core/port/secondary/src/main/java/pl/lodz/p/it/core/port/secondary/AccountRepositoryPort.java
+++ core/port/secondary/src/main/java/pl/lodz/p/it/core/port/secondary/AccountRepositoryPort.java
@@ -7,4 +7,37 @@ import pl.lodz.p.it.core.domain.Account;
  */
 public interface AccountRepositoryPort extends BaseRepositoryPort<Account> {
 
+    /**
+     * Method responsible for assigning a training plan to the user with provided login.
+     *
+     * @param login              User's business identifier
+     * @param trainingPlanNumber Training plan's business identifier
+     * @return Updated user's object.
+     */
+    Account addTrainingPlan(String login, String trainingPlanNumber);
+
+    /**
+     * Method responsible for removing a training plan from user's account.
+     *
+     * @param login              User's business identifier
+     * @param trainingPlanNumber Training plan's business identifier
+     */
+    void removeTrainingPlan(String login, String trainingPlanNumber);
+
+    /**
+     * Method responsible for assigning a diet to the user with provided login.
+     *
+     * @param login      User's business identifier
+     * @param dietNumber Diet's business identifier
+     * @return Updated user's object.
+     */
+    Account addDiet(String login, String dietNumber);
+
+    /**
+     * Method responsible for removing a diet from user's account.
+     *
+     * @param login      User's business identifier
+     * @param dietNumber Diet's business identifier
+     */
+    void removeDiet(String login, String dietNumber);
 }
