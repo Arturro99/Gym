9c448d842d2907b70ef8eb259ab9d42f3fccbd41
224363 Madaj Artur <224363@edu.p.lodz.pl>
2021-11-05 20:48:40 +0100

    Prepare mappers for handling diets

--- core/application/src/main/java/pl/lodz/p/it/core/application/secondary/mapper/DietMapper.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/secondary/mapper/DietMapper.java
@@ -1,6 +1,8 @@
 package pl.lodz.p.it.core.application.secondary.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 import pl.lodz.p.it.core.domain.Diet;
 import pl.lodz.p.it.repositoryhibernate.entity.DietEntity;
@@ -10,7 +12,19 @@ import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 /**
  * Interface responsible for mapping {@link Diet} objects and {@link DietEntity}
  */
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public abstract class DietMapper implements BaseMapper<DietEntity, Diet> {
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = DietTypeMapper.class)
+public interface DietMapper extends BaseMapper<DietEntity, Diet> {
 
+    @Override
+    @Mapping(source = "businessId", target = "number")
+    Diet toDomainModel(DietEntity entityModel);
+
+    @Override
+    @Mapping(source = "number", target = "businessId")
+    DietEntity toEntityModel(Diet domainModel);
+
+    @Override
+    @Mapping(source = "number", target = "businessId")
+    DietEntity toEntityModel(@MappingTarget DietEntity entityModel, Diet domainModel);
 }
--- core/application/src/main/java/pl/lodz/p/it/core/application/secondary/mapper/DietTypeMapper.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/secondary/mapper/DietTypeMapper.java
@@ -1,6 +1,7 @@
 package pl.lodz.p.it.core.application.secondary.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import pl.lodz.p.it.core.domain.DietType;
 import pl.lodz.p.it.repositoryhibernate.entity.DietTypeEntity;
@@ -11,6 +12,13 @@ import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
  * Interface responsible for mapping {@link DietType} objects and {@link DietTypeEntity}
  */
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public abstract class DietTypeMapper implements BaseMapper<DietTypeEntity, DietType> {
+public interface DietTypeMapper extends BaseMapper<DietTypeEntity, DietType> {
 
+    @Override
+    @Mapping(source = "businessId", target = "name")
+    DietType toDomainModel(DietTypeEntity entityModel);
+
+    @Override
+    @Mapping(source = "name", target = "businessId")
+    DietTypeEntity toEntityModel(DietType domainModel);
 }
