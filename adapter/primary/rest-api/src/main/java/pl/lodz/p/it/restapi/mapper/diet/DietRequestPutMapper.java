f56f21fdc67584599f917183f1299aba47ceb9a9
Artur Madaj <artur.madaj@atos.net>
2021-11-20 20:59:57 +0100

    Fix enum mapping

--- adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/mapper/diet/DietRequestPutMapper.java
+++ adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/mapper/diet/DietRequestPutMapper.java
@@ -1,13 +1,17 @@
 package pl.lodz.p.it.restapi.mapper.diet;
 
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import pl.lodz.p.it.core.domain.Diet;
 import pl.lodz.p.it.restapi.dto.DietRequestPut;
 import pl.lodz.p.it.restapi.mapper.BaseRequestMapper;
 
-import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
-
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE)
 public interface DietRequestPutMapper extends BaseRequestMapper<DietRequestPut, Diet> {
 
+    @Override
+    @Mapping(target = "dietType.name", source = "dietType.value")
+    Diet toDomainModel(DietRequestPut dtoModel);
 }
