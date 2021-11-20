4bc49f21cb2b496664d1aeaecacf58be5949b665
Artur Madaj <artur.madaj@atos.net>
2021-11-20 20:57:00 +0100

    Add transactional processing with retries and fix code formatting (in secondary services)

--- core/application/src/main/java/pl/lodz/p/it/core/application/secondary/service/DietTypeRepositoryService.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/secondary/service/DietTypeRepositoryService.java
@@ -1,7 +1,13 @@
 package pl.lodz.p.it.core.application.secondary.service;
 
+import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
+import static org.springframework.transaction.annotation.Propagation.MANDATORY;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.lodz.p.it.core.application.secondary.mapper.DietTypeMapper;
 import pl.lodz.p.it.core.domain.DietType;
 import pl.lodz.p.it.core.port.secondary.DietTypeRepositoryPort;
@@ -11,9 +17,12 @@ import pl.lodz.p.it.repositoryhibernate.repository.DietTypeRepository;
 /**
  * Service class responsible for operating on diet type repository.
  */
-@Component
-public class DietTypeRepositoryService extends BaseRepositoryService<DietTypeEntity, DietType> implements
-        DietTypeRepositoryPort {
+@Service
+@Transactional(propagation = MANDATORY, isolation = READ_COMMITTED, timeout = 3)
+@Retryable(maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
+public class DietTypeRepositoryService extends
+    BaseRepositoryService<DietTypeEntity, DietType> implements
+    DietTypeRepositoryPort {
 
     @Autowired
     public DietTypeRepositoryService(DietTypeRepository repository, DietTypeMapper mapper) {
