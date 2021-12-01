277bb2cbf5a706fd5137e6e1b6ab5d80776c6273
Artur Madaj <artur.madaj@atos.net>
2021-12-01 08:27:06 +0100

    Apply algorithm

--- core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/AccountService.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/AccountService.java
@@ -3,9 +3,12 @@ package pl.lodz.p.it.core.application.primary.service;
 import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
 import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.lodz.p.it.core.application.primary.service.algorithm.GoodsFactorService;
 import pl.lodz.p.it.core.domain.Account;
 import pl.lodz.p.it.core.port.primary.AccountServicePort;
 import pl.lodz.p.it.core.port.secondary.AccountRepositoryPort;
@@ -15,34 +18,43 @@ import pl.lodz.p.it.core.port.secondary.AccountRepositoryPort;
  */
 @Service
 @Transactional(propagation = REQUIRES_NEW, isolation = READ_COMMITTED)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AccountService extends BaseService<Account> implements
     AccountServicePort {
 
-    private final AccountRepositoryPort accountRepositoryPort;
+    AccountRepositoryPort accountRepositoryPort;
+
+    GoodsFactorService goodsFactorService;
 
     @Autowired
-    public AccountService(AccountRepositoryPort accountRepositoryPort) {
+    public AccountService(AccountRepositoryPort accountRepositoryPort,
+        GoodsFactorService goodsFactorService) {
         super(accountRepositoryPort);
         this.accountRepositoryPort = accountRepositoryPort;
+        this.goodsFactorService = goodsFactorService;
     }
 
     @Override
     public Account addTrainingPlan(String login, String trainingPlanNumber) {
+        goodsFactorService.increaseTrainingPlanPossessingFactor(accountRepositoryPort.find(login));
         return accountRepositoryPort.addTrainingPlan(login, trainingPlanNumber);
     }
 
     @Override
     public void removeTrainingPlan(String login, String trainingPlanNumber) {
+        goodsFactorService.decreaseTrainingPlanPossessingFactor(accountRepositoryPort.find(login));
         accountRepositoryPort.removeTrainingPlan(login, trainingPlanNumber);
     }
 
     @Override
     public Account addDiet(String login, String dietNumber) {
+        goodsFactorService.increaseDietPossessingFactor(accountRepositoryPort.find(login));
         return accountRepositoryPort.addDiet(login, dietNumber);
     }
 
     @Override
     public void removeDiet(String login, String dietNumber) {
+        goodsFactorService.decreaseDietPossessingFactor(accountRepositoryPort.find(login));
         accountRepositoryPort.removeDiet(login, dietNumber);
     }
 }
--- core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/BookingService.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/BookingService.java
@@ -9,10 +9,13 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.lodz.p.it.core.application.primary.service.util.PreferentialAlgorithm;
+import pl.lodz.p.it.core.application.primary.service.algorithm.OrderFactorService;
+import pl.lodz.p.it.core.application.primary.service.algorithm.PreferentialAlgorithmService;
+import pl.lodz.p.it.core.domain.Account;
 import pl.lodz.p.it.core.domain.Activity;
 import pl.lodz.p.it.core.domain.Booking;
 import pl.lodz.p.it.core.port.primary.BookingServicePort;
+import pl.lodz.p.it.core.port.secondary.AccountRepositoryPort;
 import pl.lodz.p.it.core.port.secondary.BookingRepositoryPort;
 
 /**
@@ -20,19 +23,28 @@ import pl.lodz.p.it.core.port.secondary.BookingRepositoryPort;
  */
 @Service
 @Transactional(propagation = REQUIRES_NEW, isolation = READ_COMMITTED)
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookingService extends BaseService<Booking> implements
     BookingServicePort {
 
-    BookingRepositoryPort bookingRepositoryPort;
+    final BookingRepositoryPort bookingRepositoryPort;
 
-    PreferentialAlgorithm algorithm;
+    final AccountRepositoryPort accountRepositoryPort;
+
+    final OrderFactorService orderFactorService;
+
+    final PreferentialAlgorithmService algorithm;
 
     @Autowired
-    public BookingService(BookingRepositoryPort bookingRepositoryPort) {
+    public BookingService(BookingRepositoryPort bookingRepositoryPort,
+        AccountRepositoryPort accountRepositoryPort,
+        OrderFactorService orderFactorService,
+        PreferentialAlgorithmService algorithm) {
         super(bookingRepositoryPort);
         this.bookingRepositoryPort = bookingRepositoryPort;
-        this.algorithm = new PreferentialAlgorithm(bookingRepositoryPort);
+        this.accountRepositoryPort = accountRepositoryPort;
+        this.orderFactorService = orderFactorService;
+        this.algorithm = algorithm;
     }
 
     @Override
@@ -53,8 +65,13 @@ public class BookingService extends BaseService<Booking> implements
     @Override
     public Booking save(Booking booking) {
         Activity activity = booking.getActivity();
+        Account account = booking.getAccount();
+
+        orderFactorService.calculateBookingOrderFactor(activity, account);
+        accountRepositoryPort.save(account);
+
         repository.save(booking);
-        if (algorithm.isActivityFull(activity)) {
+        if (orderFactorService.isActivityFull(activity)) {
             algorithm.applyPreference(activity);
         }
         return repository.save(booking);
