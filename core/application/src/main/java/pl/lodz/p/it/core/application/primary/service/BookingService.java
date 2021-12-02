63ebf255ab897385e285b251f7bc6c4b6f00261c
Artur Madaj <artur.madaj@atos.net>
2021-12-02 11:35:08 +0100

    Fix order-side algorithm

--- core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/BookingService.java
+++ core/application/src/main/java/pl/lodz/p/it/core/application/primary/service/BookingService.java
@@ -1,8 +1,10 @@
 package pl.lodz.p.it.core.application.primary.service;
 
 import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
+import static org.springframework.transaction.annotation.Propagation.REQUIRED;
 import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
@@ -16,6 +18,7 @@ import pl.lodz.p.it.core.domain.Activity;
 import pl.lodz.p.it.core.domain.Booking;
 import pl.lodz.p.it.core.port.primary.BookingServicePort;
 import pl.lodz.p.it.core.port.secondary.AccountRepositoryPort;
+import pl.lodz.p.it.core.port.secondary.ActivityRepositoryPort;
 import pl.lodz.p.it.core.port.secondary.BookingRepositoryPort;
 
 /**
@@ -35,16 +38,20 @@ public class BookingService extends BaseService<Booking> implements
 
     final PreferentialAlgorithmService algorithm;
 
+    final ActivityRepositoryPort activityRepositoryPort;
+
     @Autowired
     public BookingService(BookingRepositoryPort bookingRepositoryPort,
         AccountRepositoryPort accountRepositoryPort,
         OrderFactorService orderFactorService,
-        PreferentialAlgorithmService algorithm) {
+        PreferentialAlgorithmService algorithm,
+        ActivityRepositoryPort activityRepositoryPort) {
         super(bookingRepositoryPort);
         this.bookingRepositoryPort = bookingRepositoryPort;
         this.accountRepositoryPort = accountRepositoryPort;
         this.orderFactorService = orderFactorService;
         this.algorithm = algorithm;
+        this.activityRepositoryPort = activityRepositoryPort;
     }
 
     @Override
@@ -64,15 +71,20 @@ public class BookingService extends BaseService<Booking> implements
 
     @Override
     public Booking save(Booking booking) {
-        Activity activity = booking.getActivity();
-        Account account = booking.getAccount();
+        Activity activity = activityRepositoryPort.find(booking.getActivity().getNumber());
+        Account account = accountRepositoryPort.find(booking.getAccount().getLogin());
 
-        orderFactorService.calculateBookingOrderFactor(activity, account);
-        accountRepositoryPort.save(account);
+        booking.setActivity(activity);
+        booking.setAccount(account);
+        booking.setActive(true);
+        booking.setCompleted(false);
+        booking.setPending(false);
+        booking.setCreationDate(OffsetDateTime.now());
 
-        repository.save(booking);
-        if (orderFactorService.isActivityFull(activity)) {
-            algorithm.applyPreference(activity);
+        orderFactorService.calculateBookingOrderFactor(booking);
+
+        if (orderFactorService.isActivityFull(activity, booking)) {
+            algorithm.applyPreference(activity, booking);
         }
         return repository.save(booking);
     }
