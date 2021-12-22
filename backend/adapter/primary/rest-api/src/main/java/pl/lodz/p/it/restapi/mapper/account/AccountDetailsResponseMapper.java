b8465b01c7b0c0c8d5a049041505e18d8ba334ea
Artur Madaj <artur.madaj@atos.net>
2021-12-22 17:56:08 +0100

    Init account-diet and account-trainingPlan relations

--- backend/adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/mapper/account/AccountDetailsResponseMapper.java
+++ backend/adapter/primary/rest-api/src/main/java/pl/lodz/p/it/restapi/mapper/account/AccountDetailsResponseMapper.java
@@ -1,15 +1,15 @@
 package pl.lodz.p.it.restapi.mapper.account;
 
+import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+
 import org.mapstruct.Mapper;
 import pl.lodz.p.it.core.domain.Account;
 import pl.lodz.p.it.restapi.dto.AccountDetailsResponse;
 import pl.lodz.p.it.restapi.mapper.BaseResponseMapper;
+import pl.lodz.p.it.restapi.mapper.diet.DietResponseMapper;
 
-import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE, uses = DietResponseMapper.class)
+public interface AccountDetailsResponseMapper extends
+    BaseResponseMapper<AccountDetailsResponse, Account> {
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = IGNORE)
-public interface AccountDetailsResponseMapper extends BaseResponseMapper<AccountDetailsResponse, Account> {
-
-    @Override
-    AccountDetailsResponse toDtoModel(Account domainModel);
 }
