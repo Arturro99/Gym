d6641bd3f66242e4154dfa31883f1825c10ace2e
Artur Madaj <artur.madaj@atos.net>
2022-01-04 18:34:00 +0100

    Implement JWT refreshing

--- backend/service-starter/src/main/java/pl/lodz/p/it/gymbackend/config/JWTRequestFilter.java
+++ backend/service-starter/src/main/java/pl/lodz/p/it/gymbackend/config/JWTRequestFilter.java
@@ -1,8 +1,15 @@
 package pl.lodz.p.it.gymbackend.config;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,12 +18,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import pl.lodz.p.it.core.shared.SecurityConstants;
 import pl.lodz.p.it.core.shared.exception.SecurityException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 @AllArgsConstructor
@@ -27,28 +28,38 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     private final JWTUtils jwtUtils;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
 
         final String authorizationHeader = request.getHeader("Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Refresh-Token");
 
         String jwt = null;
         String username = null;
+        String newToken;
 
         try {
-            if (authorizationHeader != null && authorizationHeader.startsWith(SecurityConstants.BEARER)) {
+            if (authorizationHeader != null && authorizationHeader
+                .startsWith(SecurityConstants.BEARER)) {
                 jwt = authorizationHeader.substring(SecurityConstants.BEARER.length()).trim();
                 username = jwtUtils.extractUsername(jwt);
             }
 
-            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (username != null
+                && SecurityContextHolder.getContext().getAuthentication() == null) {
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 if (jwtUtils.validateToken(jwt, userDetails)) {
 
                     UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        new UsernamePasswordAuthenticationToken(userDetails, null,
+                            userDetails.getAuthorities());
                     usernamePasswordAuthenticationToken
-                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext()
+                        .setAuthentication(usernamePasswordAuthenticationToken);
+                    newToken = jwtUtils.refreshToken(userDetails.getUsername(),
+                        (List<GrantedAuthority>) userDetails.getAuthorities());
+                    response.addHeader("Refresh-Token", newToken);
                 }
             }
             filterChain.doFilter(request, response);
--- frontend/src/services/HttpService.js
+++ frontend/src/services/HttpService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify'
 import { attachToken } from "./AuthenticationService";
+import jwt from "jwt-decode";
 
 axios.interceptors.response.use(null, error => {
   const expectedError = error.response
@@ -15,9 +16,21 @@ axios.interceptors.response.use(null, error => {
 });
 
 axios.defaults.headers.post['Content-Type'] = 'application/json'
-axios.interceptors.request.use(function(config) {
+axios.interceptors.request.use(config => {
   const token = attachToken();
   config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
+axios.interceptors.response.use(config => {
+  const token = config.headers["refresh-token"];
+  if (token) {
+    const values = jwt(token)
+    localStorage.setItem('token', token);
+    localStorage.setItem('userName', values.sub);
+    localStorage.setItem('exp', values.exp);
+    localStorage.setItem('roles', values.auth);
+    localStorage.setItem('currentRole', values.auth.split(',')[0]);
+  }
   return config;
 })
 
